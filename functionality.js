ccb0de704c361446e471d33b3a596f6e4491d0fa
SpyrosAliv <spyrosalivizatos8@gmail.com>
2023-05-17 16:59:56 +0300

    Finished mobile interface and functionality

---
+++ functionality.js
@@ -0,0 +1,7 @@
+const openMenu = () => {
+    document.getElementById("open-menu").style.display = "block";
+}
+
+const closeMenu = () => {
+    document.getElementById("open-menu").style.display = "none";
+}
